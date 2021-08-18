155cc540bf4c874594303ad24e232902f9519425
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-08-18 17:48:20 -0300

    refatorado

--- s_katas-6.js
+++ s_katas-6.js
@@ -1,13 +1,17 @@
 /* 
  * 1. Convert this to an arrow function
  */
-const add = (x, y) => x+y
+const add = (x, y) => {
+    return x+y
+}
 
 /*
  * 2. If a function only has one argument, you can exclude the parentheses.
  *    Convert this to an arrow function that takes advantage of that.
  */
-const increment = number => number + 1
+const increment = number => {
+    return number + 1
+}
 
 /* 
  * 3. If a function's body includes a single expression, you can drop the curly
